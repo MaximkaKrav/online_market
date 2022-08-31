64566d7aed702fb363fb090a402e19479055307b
MaximkaKrav <48203504+MaximkaKrav@users.noreply.github.com>
2022-08-31 15:29:32 +0300

    Test

---
+++ online-market/server/models/models.js
@@ -0,0 +1,8 @@
+const sequalize = require('./db')
+const {DataTypes} = require('sequelize')
+
+
+const User = sequalize.define('user',{
+    id: {type : DataTypes.INTEGER,primaryKey : true, autoIncrement: true},
+    email: {type : DataTypes.STRING,primaryKey : true},
+})
